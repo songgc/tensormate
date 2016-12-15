12e4a95e5f7660aabe3394c8e956568794ff3377
Guocong Song <songgc@gmail.com>
2016-12-15 11:47:10 -0800

    Fix setup bugs

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='tensormate',
     version='0.1',
-    packages=['tensormate'],
-    url='/home/guocong/git/bitbucket/tf_image_classification/common_ops/py_utils.py',
+    packages=['tensormate', 'tensormate.graph', 'tensormate.proto'],
+    url='https://github.com/songgc/tensormate.git',
     license='Apache 2.0',
-    author='guocong',
+    author='Guocong Song',
     author_email='songgc@ieee.org',
     description='Utility tools for tensorflow '
 )
