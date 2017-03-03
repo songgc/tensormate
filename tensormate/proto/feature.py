089c081ce54ad75be377afbcca60f507c7e09783
Guocong Song <songgc@gmail.com>
2017-03-03 10:02:26 -0800

    Add sequence features and related refractory

--- tensormate/proto/feature.py
+++ tensormate/proto/feature.py
@@ -118,63 +118,6 @@ class SparseBytesFeature(SparseFeature):
         return Feature.bytes_feature(value)
 
 
-class FeaturesMeta(type):
-    ORDER = "_order"
-
-    def __new__(mcs, clsname, bases, clsdict):
-        d = dict(clsdict)
-        order = []
-        for base in reversed(bases):
-            if issubclass(base, Features) and base is not Features:
-                base_order = base.__dict__.get(mcs.ORDER)
-                for name in base_order:
-                    val = base.__dict__.get(name)
-                    d[name] = val
-                order = base_order + order
-        for name, value in clsdict.items():
-            if isinstance(value, Feature):
-                value._name = name
-                order.append(name)
-        d[mcs.ORDER] = order
-        return type.__new__(mcs, clsname, bases, d)
-
-    @classmethod
-    def __prepare__(mcs, name, bases):
-        return OrderedDict()
-
-
-class Features(metaclass=FeaturesMeta):
-    @classmethod
-    def get_all_feature_names(cls):
-        return list(cls.__dict__.get(cls.ORDER))
-
-    @classmethod
-    def feature_map(cls):
-        d = OrderedDict()
-        for name in cls.__dict__.get(cls.ORDER):
-            feature = cls.__dict__.get(name)
-            d[feature.name] = feature.parse_type
-        return d
-
-    @classmethod
-    def to_tf_feature(cls, fea, value):
-        """
-        TODO: remove
-        """
-        if isinstance(fea, Feature):
-            tf_feature = fea(value)
-        elif isinstance(fea, str):
-            feaObj = cls.__dict__.get(fea)
-            tf_feature = feaObj(value)
-        else:
-            raise TypeError("feature")
-        return tf_feature
-
-    @classmethod
-    def to_pb_features(cls, feature_tuples):
-        return tf.train.Features(feature=dict(feature_tuples))
-
-
 class FeatureList(object):
 
     def __init__(self, name, dtype, shape=[], allow_missing=True, replace=None):
@@ -188,8 +131,6 @@ class FeatureList(object):
     @staticmethod
     def int64_feature(values):
         """Wrapper for inserting int64 features into."""
-        if not isinstance(values, list):
-            raise ""
         fl = tf.train.FeatureList()
         for value in values:
             field = fl.feature.add()
@@ -199,8 +140,6 @@ class FeatureList(object):
     @staticmethod
     def float_feature(values):
         """Wrapper for inserting float features into"""
-        if not isinstance(values, list):
-            raise ""
         fl = tf.train.FeatureList()
         for value in values:
             field = fl.feature.add()
@@ -210,13 +149,11 @@ class FeatureList(object):
     @staticmethod
     def bytes_feature(values):
         """Wrapper for inserting bytes features into"""
-        if not isinstance(values, list):
-            raise ""
         fl = tf.train.FeatureList()
         for value in values:
-            field = fl.feature.add()
             if isinstance(value, str):
                 value = str.encode(value)
+            field = fl.feature.add()
             field.bytes_list.value.append(value)
         return fl
 
@@ -267,3 +204,117 @@ class BytesFeatureList(FeatureList):
     @staticmethod
     def _encode_fun(values):
         return FeatureList.bytes_feature(values)
+
+
+class FeaturesMeta(type):
+    ORDER = "_order"
+
+    def __new__(mcs, clsname, bases, clsdict):
+        d = dict(clsdict)
+        order = []
+        for base in reversed(bases):
+            if issubclass(base, Features) and base is not Features \
+                    or issubclass(base, SequenceFeatures) and base is not SequenceFeatures:
+                base_order = base.__dict__.get(mcs.ORDER)
+                for name in base_order:
+                    val = base.__dict__.get(name)
+                    d[name] = val
+                order = base_order + order
+        for name, value in clsdict.items():
+            if isinstance(value, Feature) or isinstance(value, FeatureList):
+                value._name = name
+                order.append(name)
+        d[mcs.ORDER] = order
+        return type.__new__(mcs, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(mcs, name, bases):
+        return OrderedDict()
+
+
+class Features(metaclass=FeaturesMeta):
+    @classmethod
+    def all_feature_names(cls):
+        return list(cls.__dict__.get(cls.ORDER))
+
+    @classmethod
+    def feature_map(cls):
+        d = OrderedDict()
+        for name in cls.__dict__.get(cls.ORDER):
+            feature = cls.__dict__.get(name)
+            d[feature.name] = feature.parse_type
+        return d
+
+    @classmethod
+    def to_tf_feature(cls, fea, value):
+        """
+        TODO: remove
+        """
+        if isinstance(fea, Feature):
+            tf_feature = fea(value)
+        elif isinstance(fea, str):
+            feaObj = cls.__dict__.get(fea)
+            tf_feature = feaObj(value)
+        else:
+            raise TypeError("feature")
+        return tf_feature
+
+    @classmethod
+    def to_pb_features(cls, feature_tuples):
+        return tf.train.Features(feature=dict(feature_tuples))
+
+    @classmethod
+    def to_pb_example(cls, feature_tuples):
+        features = cls.to_pb_features(feature_tuples)
+        return tf.train.Example(features=features)
+
+
+class SequenceFeatures(metaclass=FeaturesMeta):
+    @classmethod
+    def all_feature_names(cls):
+        return list(cls.__dict__.get(cls.ORDER))
+
+    @classmethod
+    def context_feature_names(cls):
+        context_features = [name for name in cls.__dict__.get(cls.ORDER)
+                            if isinstance(cls.__dict__.get(name), Feature)]
+        return context_features
+
+    @classmethod
+    def feature_list_names(cls):
+        feature_lists = [name for name in cls.__dict__.get(cls.ORDER)
+                         if isinstance(cls.__dict__.get(name), FeatureList)]
+        return feature_lists
+
+    @classmethod
+    def context_feature_map(cls):
+        d = OrderedDict()
+        for name in cls.context_feature_names():
+            feature = cls.__dict__.get(name)
+            d[feature.name] = feature.parse_type
+        return d
+
+    @classmethod
+    def feature_list_map(cls):
+        d = OrderedDict()
+        for name in cls.feature_list_names():
+            feature = cls.__dict__.get(name)
+            d[feature.name] = feature.parse_type
+        return d
+
+    @classmethod
+    def to_pb_features(cls, feature_tuples):
+        return tf.train.Features(feature=dict(feature_tuples))
+
+    @classmethod
+    def to_pb_feature_lists(cls, feature_list_tuples):
+        return tf.train.FeatureList(feature_list=dict(feature_list_tuples))
+
+    @classmethod
+    def to_pb_sequence_example(cls, feature_tuples=None, feature_list_tuples=None):
+        kwargs = dict()
+        if not feature_tuples:
+            kwargs["context"] = cls.to_pb_features(feature_tuples)
+        if not feature_list_tuples:
+            kwargs["feature_list"] = cls.to_pb_feature_lists(feature_list_tuples)
+        return tf.train.SequenceExample(**kwargs)
