07087f0f3c6f444f62dad1964c26b8add532d766
Guocong Song <songgc@gmail.com>
2017-03-07 10:15:49 -0800

    Refactor unit test and add a sparse feature in the test

--- tensormate/proto/features_test.py
+++ tensormate/proto/features_test.py
@@ -13,55 +13,61 @@ class TestFeatures(features.Features):
     feature_a = features.Int64Feature(replace={"_": "/"})
     feature_b = features.Float32Feature()
     feature_c = features.BytesFeature(replace={"_": "/"})
+    feature_sparse = features.SparseInt64Feature()
 
 
 class FeaturesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.value_a = 1
+        self.value_b = -1.0
+        self.value_c = b"Hello"
+        self.value_sparse = [11, 12]
 
     def test_name(self):
         self.assertEqual(TestFeatures.feature_a.name, "feature/a")
         self.assertEqual(TestFeatures.feature_b.name, "feature_b")
 
     def test_example_encode_and_decode(self):
-        value_a = 1
-        value_b = -1.0
-        value_c = b"Hello"
         # encode
         feature_tuples = [
-            TestFeatures.feature_a(value_a),
-            TestFeatures.feature_b(value_b),
-            TestFeatures.feature_c(value_c),
+            TestFeatures.feature_a(self.value_a),
+            TestFeatures.feature_b(self.value_b),
+            TestFeatures.feature_c(self.value_c),
+            TestFeatures.feature_sparse(self.value_sparse)
         ]
         example = TestFeatures.to_pb_example(feature_tuples)
         # decode
         parsed = tf.parse_single_example(serialized=example.SerializeToString(), features=TestFeatures.feature_map())
         values = _run_tf_session(parsed)
-        self.assertEqual(values[TestFeatures.feature_a.name], value_a)
-        self.assertEqual(values[TestFeatures.feature_b.name], value_b)
-        self.assertEqual(values[TestFeatures.feature_c.name], value_c)
+        self.assertEqual(values[TestFeatures.feature_a.name], self.value_a)
+        self.assertEqual(values[TestFeatures.feature_b.name], self.value_b)
+        self.assertEqual(values[TestFeatures.feature_c.name], self.value_c)
+        self.assertEqual(values[TestFeatures.feature_sparse.name].values.tolist(), self.value_sparse)
 
     def test_example_use_old_way(self):
-        value_a = 1
-        value_b = -1.0
-        value_c = b"Hello"
         # encode
         feature_tuples = [
-            ("feature/a", tf.train.Feature(int64_list=tf.train.Int64List(value=[value_a]))),
-            ("feature_b", tf.train.Feature(float_list=tf.train.FloatList(value=[value_b]))),
-            ("feature/c", tf.train.Feature(bytes_list=tf.train.BytesList(value=[value_c]))),
+            ("feature/a", tf.train.Feature(int64_list=tf.train.Int64List(value=[self.value_a]))),
+            ("feature_b", tf.train.Feature(float_list=tf.train.FloatList(value=[self.value_b]))),
+            ("feature/c", tf.train.Feature(bytes_list=tf.train.BytesList(value=[self.value_c]))),
+            ("feature_sparse", tf.train.Feature(int64_list=tf.train.Int64List(value=self.value_sparse))),
         ]
-        features = tf.train.Features(feature=dict(feature_tuples))
-        example = tf.train.Example(features=features)
+        proto_features = tf.train.Features(feature=dict(feature_tuples))
+        example = tf.train.Example(features=proto_features)
         # decode
         feature_map = {
             "feature/a": tf.FixedLenFeature(shape=[], dtype=tf.int64, default_value=-1),
             "feature_b": tf.FixedLenFeature(shape=[], dtype=tf.float32, default_value=-1),
             "feature/c": tf.FixedLenFeature(shape=[], dtype=tf.string, default_value=""),
+            "feature_sparse": tf.VarLenFeature(dtype=tf.int64),
         }
         parsed = tf.parse_single_example(serialized=example.SerializeToString(), features=feature_map)
         values = _run_tf_session(parsed)
-        self.assertEqual(values["feature/a"], value_a)
-        self.assertEqual(values["feature_b"], value_b)
-        self.assertEqual(values["feature/c"], value_c)
+        self.assertEqual(values["feature/a"], self.value_a)
+        self.assertEqual(values["feature_b"], self.value_b)
+        self.assertEqual(values["feature/c"], self.value_c)
+        self.assertEqual(values["feature_sparse"].values.tolist(), self.value_sparse)
 
 
 class TestSequenceFeatures(features.SequenceFeatures):
@@ -72,15 +78,18 @@ class TestSequenceFeatures(features.SequenceFeatures):
 
 class SequenceFeaturesTest(unittest.TestCase):
 
+    def setUp(self):
+        self.tokens = [1, 2, 3]
+        self.labels = [0, 1, 0]
+
     def test_sequence_example(self):
-        tokens, labels = [1, 2, 3], [0, 1, 0]
         # encode
         feature_tuples = [
-            TestSequenceFeatures.length(len(tokens)),
+            TestSequenceFeatures.length(len(self.tokens)),
         ]
         feature_list_tuples = [
-            TestSequenceFeatures.tokens(tokens),
-            TestSequenceFeatures.labels(labels),
+            TestSequenceFeatures.tokens(self.tokens),
+            TestSequenceFeatures.labels(self.labels),
         ]
         seq_ex = TestSequenceFeatures.to_pb_sequence_example(feature_tuples=feature_tuples,
                                                              feature_list_tuples=feature_list_tuples)
@@ -91,19 +100,18 @@ class SequenceFeaturesTest(unittest.TestCase):
             sequence_features=TestSequenceFeatures.feature_list_map()
         )
         context, sequence = _run_tf_session([context_parsed, sequence_parsed])
-        self.assertEqual(context[TestSequenceFeatures.length.name], len(tokens))
-        self.assertEqual(sequence[TestSequenceFeatures.tokens.name].tolist(), tokens)
-        self.assertEqual(sequence[TestSequenceFeatures.labels.name].tolist(), labels)
+        self.assertEqual(context[TestSequenceFeatures.length.name], len(self.tokens))
+        self.assertEqual(sequence[TestSequenceFeatures.tokens.name].tolist(), self.tokens)
+        self.assertEqual(sequence[TestSequenceFeatures.labels.name].tolist(), self.labels)
 
     def test_sequence_example_use_old_way(self):
-        tokens, labels = [1, 2, 3], [0, 1, 0]
         # encode
         seq_ex = tf.train.SequenceExample()
-        sequence_length = len(tokens)
+        sequence_length = len(self.tokens)
         seq_ex.context.feature["length"].int64_list.value.append(sequence_length)
         fl_tokens = seq_ex.feature_lists.feature_list["tokens"]
         fl_labels = seq_ex.feature_lists.feature_list["labels"]
-        for token, label in zip(tokens, labels):
+        for token, label in zip(self.tokens, self.labels):
             fl_tokens.feature.add().int64_list.value.append(token)
             fl_labels.feature.add().int64_list.value.append(label)
         # decode
@@ -120,9 +128,9 @@ class SequenceFeaturesTest(unittest.TestCase):
             sequence_features=sequence_features
         )
         context, sequence = _run_tf_session([context_parsed, sequence_parsed])
-        self.assertEqual(context["length"], len(tokens))
-        self.assertEqual(sequence["tokens"].tolist(), tokens)
-        self.assertEqual(sequence["labels"].tolist(), labels)
+        self.assertEqual(context["length"], len(self.tokens))
+        self.assertEqual(sequence["tokens"].tolist(), self.tokens)
+        self.assertEqual(sequence["labels"].tolist(), self.labels)
 
 
 if __name__ == '__main__':
